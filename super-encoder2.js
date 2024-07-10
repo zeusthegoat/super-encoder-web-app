bad960197e8b05d71bfc2fb24f59c4f2eb79b6a1
zeusthegoat <niklausantonio@gmail.com>
2024-07-10 14:44:38 +0100

    Update super-encoder2.js

--- super-encoder2.js
+++ super-encoder2.js
@@ -66,4 +66,4 @@ const caesarCipher = (str, amount = 0) => {
     return reverseCipher(symbolCipher(caesarCipher(str, -8)));
   };
 window.encodeMessage = encodeMessage;
-window.decodeMessage = decodeMessade;
+window.decodeMessage = decodeMessage;
